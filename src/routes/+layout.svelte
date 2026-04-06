6bbb42d2f4edd38294c1e9bba3d3c2c6a6ffae66
P1T0NN <ognjen.tapuskovic@gmail.com>
2026-04-06 16:23:26 +0200

    Added Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,20 @@
 	import './layout.css';
 	import favicon from '@/shared/lib/assets/favicon.svg';
 
+	// SVELTEKIT IMPORTS
+	import { dev } from '$app/environment';
+
+	// LIBRARIES
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
 	// COMPONENTS
 	import { Toaster } from '@/shared/components/ui/sonner';
 	import Footer from '@/shared/components/ui/footer/footer.svelte';
 	import Header from '@/shared/components/ui/header/header.svelte';
 
 	let { children } = $props();
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
